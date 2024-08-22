8de55bf36850ca99d31a6ebc2676a5c9199c105b
Unai Perez <unaipdd@gmail.com>
2024-08-22 18:30:04 +0200

    trying to fix

--- base.h
+++ base.h
@@ -39,22 +39,29 @@ void makedir(const char* dir_name){
     if (stat(dir_name, &st) == -1) {
         // The directory does not exist, so try to create it
         #ifdef _WIN32
-            if (mkdir(dir_name) != 0) { 
-                perror("Couldn't make directory");
-                exit(EXIT_FAILURE);
+            if (_mkdir(dir_name) != 0) {
+                if (errno != EEXIST) {
+                    perror("Couldn't make directory");
+                    exit(EXIT_FAILURE);
+                }
             }
         #else
-            if (mkdir(dir_name, 0755) != 0) { 
-                perror("Couldn't make directory");
-                exit(EXIT_FAILURE);
+            if (mkdir(dir_name, 0755) != 0) {
+                if (errno != EEXIST) {
+                    perror("Couldn't make directory");
+                    exit(EXIT_FAILURE);
+                }
             }
         #endif
+    } else if (!S_ISDIR(st.st_mode)) {
+        fprintf(stderr, "Path exists but is not a directory: %s\n", dir_name);
+        exit(EXIT_FAILURE);
     }
 }
 
 char* catpath(const char* subdir, const char* file){
     static char fullpath[PATHSIZE];
-    if (sprintf(fullpath, "%s/%s", subdir, file) < 0) {
+    if (snprintf(fullpath, PATHSIZE, "%s/%s", subdir, file) >= PATHSIZE) {
         fprintf(stderr, "Error concatenating path and file name.\n");
         exit(EXIT_FAILURE);
     }
@@ -62,8 +69,9 @@ char* catpath(const char* subdir, const char* file){
 }
 
 bool CheckDataSet(const char* tablename){
-    struct stat buffer;
-    if (stat(tablename, &buffer) == 0){
+    FILE* file = fopen(tablename, "r");
+    if (file) {
+        fclose(file);
         return true; // Success
     } else {
         return false;
